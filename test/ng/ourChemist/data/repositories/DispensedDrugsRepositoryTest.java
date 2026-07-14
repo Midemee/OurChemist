b72c215ca03fc4275e12aeb419ae0d66a09c076d
Midemee <ashiwajuaramide@gmail.com>
2026-07-14 23:32:50 +0100

    Update

--- test/ng/ourChemist/data/repositories/DispensedDrugsRepositoryTest.java
+++ test/ng/ourChemist/data/repositories/DispensedDrugsRepositoryTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DispensedDrugssRepositoryTest {
+class DispensedDrugsRepositoryTest {
     private DispensedDrugsRepository dispensedDrugsRepository;
 
     @BeforeEach
